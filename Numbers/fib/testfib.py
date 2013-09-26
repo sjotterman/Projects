2e18695af34d4f33d1d38d736590c57c520b40ab
Samuel Otterman <sjotterman@gmail.com>
2013-09-26 11:42:20 -0400

    added test cases

--- Numbers/fib/testfib.py
+++ Numbers/fib/testfib.py
@@ -14,5 +14,9 @@ class TestFib(unittest.TestCase):
     def test_fib_2(self):
         self.assertEqual(fib(2), 1)
 
+    def test_fib_range(self):
+        for x in range(2,10):
+            self.assertEqual(fib(x), fib(x - 1) + fib(x - 2))
+
 if __name__ == "__main__":
        unittest.main()
