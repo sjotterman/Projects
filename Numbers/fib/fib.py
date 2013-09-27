bd94dc53acd45a5e36cf5d5a6df3df59c9b1e745
Samuel Otterman <sjotterman@gmail.com>
2013-09-27 14:14:02 -0400

    fib works for n <= 30

--- Numbers/fib/fib.py
+++ Numbers/fib/fib.py
@@ -4,7 +4,11 @@
 import sys
 
 def fib(n):
-    return n        
+    if (n == 0):
+        return 0
+    if (n == 1):
+        return 1    
+    return fib(n -1) + fib(n - 2)        
 
 def main():
    if (len(sys.argv) == 2):    
--- Numbers/fib/test_fib.py
+++ Numbers/fib/test_fib.py
@@ -11,12 +11,12 @@ class TestFib(unittest.TestCase):
     def test_fib_1(self):
         self.assertEqual(fib(1), 1)
 
-#    def test_fib_2(self):
-#        self.assertEqual(fib(2), 1)
+    def test_fib_2(self):
+        self.assertEqual(fib(2), 1)
 
- #   def test_fib_range(self):
- #       for x in range(2,10):
- #           self.assertEqual(fib(x), fib(x - 1) + fib(x - 2))
+    def test_fib_range(self):
+        for x in range(2,30):
+            self.assertEqual(fib(x), fib(x - 1) + fib(x - 2))
 
 if __name__ == "__main__":
        unittest.main()
