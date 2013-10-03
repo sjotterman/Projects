aa8e8d92b9f1b64dd8dd9cf947ac115f71a85dd1
Samuel Otterman <sjotterman@gmail.com>
2013-10-03 14:42:34 -0400

    changed tabs to spaces in test_pi_digit

--- Numbers/pi_digit/test_pi_digit.py
+++ Numbers/pi_digit/test_pi_digit.py
@@ -5,26 +5,26 @@ import unittest
 from pi_digit import pi_digit
 
 class Test_pi_digit(unittest.TestCase):
-	def test_pi_digit_1(self):
-		self.assertEqual(pi_digit(1), 3)
+    def test_pi_digit_1(self):
+        self.assertEqual(pi_digit(1), 3)
 
-	def test_pi_digit_2(self):
-		self.assertEqual(pi_digit(2), 1)
+    def test_pi_digit_2(self):
+        self.assertEqual(pi_digit(2), 1)
 
-	def test_pi_digit_3(self):
-		self.assertEqual(pi_digit(3), 4)
+    def test_pi_digit_3(self):
+        self.assertEqual(pi_digit(3), 4)
 
-	def test_pi_digit_4(self):
-		self.assertEqual(pi_digit(4), 1)
+    def test_pi_digit_4(self):
+        self.assertEqual(pi_digit(4), 1)
 
-	def test_pi_digit_5(self):
-		self.assertEqual(pi_digit(5), 5)
+    def test_pi_digit_5(self):
+        self.assertEqual(pi_digit(5), 5)
 
-	def test_pi_digit_6(self):
-		self.assertEqual(pi_digit(6), 9)
+    def test_pi_digit_6(self):
+        self.assertEqual(pi_digit(6), 9)
 
     def test_pi_digit_15(self):
-        self.assertEqual(pi_digit(15), 9)    
+        self.assertEqual(pi_digit(15), 9)
 
 if __name__ == "__main__":
    unittest.main()
