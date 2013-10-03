0725384d10ad77da7053cc19349b3cff9ffef3a7
Samuel Otterman <sjotterman@gmail.com>
2013-10-03 15:10:18 -0400

    pi_digit works for n <= 15

--- Numbers/pi_digit/pi_digit.py
+++ Numbers/pi_digit/pi_digit.py
@@ -2,11 +2,12 @@
 #pi_digit.py
 
 import sys
+from decimal import *
 
 def pi_digit(n):
 	if (n == 1):
 		return 3
-	full_pi = 3.141591
-	stripped_pi = str(full_pi)
+	full_pi = Decimal(3.141592653589793238462643383279)
+	stripped_pi = str(full_pi).strip('.')
 	str_digit = stripped_pi[n:n + 1]
 	return int(str_digit)
