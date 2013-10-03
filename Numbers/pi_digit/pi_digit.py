6141503bc3d86b3c4faa565be173a4b223cb7b15
Samuel Otterman <sjotterman@gmail.com>
2013-10-03 16:01:34 -0400

    in pi_digit, pi is first stored as a string

--- Numbers/pi_digit/pi_digit.py
+++ Numbers/pi_digit/pi_digit.py
@@ -7,7 +7,7 @@ from decimal import *
 def pi_digit(n):
 	if (n == 1):
 		return 3
-	full_pi = Decimal(3.141592653589793238462643383279)
+	full_pi = Decimal('3.141592653589793238462643383279')
 	stripped_pi = str(full_pi).strip('.')
 	str_digit = stripped_pi[n:n + 1]
 	return int(str_digit)
