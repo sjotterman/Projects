d1c5dda2f4ccfcfa67d783088caf726ad57ea7d4
Samuel Otterman <sjotterman@gmail.com>
2013-10-04 09:27:54 -0400

    trying to run nosetests3 for newer versions of python

--- test.sh
+++ test.sh
@@ -1,3 +1,10 @@
 #/usr/bin/sh
 
-nosetests
+nosetests -w Numbers
+
+ret=`python -c 'import sys; print("%i" % (sys.hexversion<0x03000000))'`
+if [ $ret -eq 0 ]; then
+   nosetests3 -w Numbers
+else 
+    nosetests -w Numbers
+fi
