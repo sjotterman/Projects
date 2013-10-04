796074647ce67d86237e29a8186edc60b95de6fb
Samuel Otterman <sjotterman@gmail.com>
2013-10-04 09:43:20 -0400

    reverted changes to test.sh

--- test.sh
+++ test.sh
@@ -1,10 +1,3 @@
 #/usr/bin/sh
 
 nosetests -w Numbers
-
-ret=`python -c 'import sys; print("%i" % (sys.hexversion<0x03000000))'`
-if [ $ret -eq 0 ]; then
-   nosetests3 -w Numbers
-else 
-    nosetests -w Numbers
-fi
