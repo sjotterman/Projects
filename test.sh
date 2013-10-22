f77452e609ab42226ebc05ccc3e0e2861ce512fe
Samuel Otterman <sjotterman@gmail.com>
2013-10-22 13:48:12 -0400

    trying a version of the test script to use nosetests3

--- test.sh
+++ test.sh
@@ -1,3 +1,11 @@
 #/usr/bin/sh
 
-nosetests -w Numbers
+case "$(python --version 2>&1)" in
+    *" 3."*)
+        nosetests3 -w Numbers
+        ;;
+    *)
+        nosetests -w Numbers
+        ;;
+esac
+
