31905099d10fad74e7ebaef0960bb85fa6426088
Samuel Otterman <sjotterman@gmail.com>
2013-10-25 14:15:02 -0400

    trying different import statement

--- Numbers/fib/test_fib.py
+++ Numbers/fib/test_fib.py
@@ -1,22 +1,24 @@
 #! /usr/bin/python3
 #testfib.py
 
+
+from __future__ import absolute_import
 import unittest
-from fib import fib
+from . import fib
 
 class TestFib(unittest.TestCase):
     def test_fib_0(self):
-        self.assertEqual(fib(0), 0)
+        self.assertEqual(fib.fib(0), 0)
 
     def test_fib_1(self):
-        self.assertEqual(fib(1), 1)
+        self.assertEqual(fib.fib(1), 1)
 
     def test_fib_2(self):
-        self.assertEqual(fib(2), 1)
+        self.assertEqual(fib.fib(2), 1)
 
     def test_fib_range(self):
         for x in range(2,13):
-            self.assertEqual(fib(x), fib(x - 1) + fib(x - 2))
+            self.assertEqual(fib.fib(x), fib.fib(x - 1) + fib.fib(x - 2))
 
 if __name__ == "__main__":
        unittest.main()
--- test.sh
+++ test.sh
@@ -1,12 +1,4 @@
 #/usr/bin/sh
 
-case "$(python --version 2>&1)" in
-    *" 3."*)
-        pip install nose3
-        nosetests3 -w Numbers
-        ;;
-    *)
-        nosetests -w Numbers
-        ;;
-esac
+nosetests -w Numbers
 
