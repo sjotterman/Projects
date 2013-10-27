072dcd01e8b9a44de7b2fec20d20047cc154068d
Samuel Otterman <sjotterman@gmail.com>
2013-10-27 18:03:39 -0400

    Added main to factorial

--- Numbers/factorial/factorial.py
+++ Numbers/factorial/factorial.py
@@ -1,4 +1,7 @@
+#! /usr/bin/python
 ##factorial.py
+
+import sys
 
 def fact_rec(n):
     if (n == 0):
@@ -18,3 +21,12 @@ def fact_it(n):
     for i in range(1, n + 1):
         sum *= i
     return sum
+
+def main():
+   if (len(sys.argv) == 2):
+       print(fact_it(sys.argv[1]))
+   else:
+       print("Usage: ", sys.argv[0], " [number]")
+
+if __name__ == "__main__":
+        main()
--- Numbers/fib/fib.py
+++ Numbers/fib/fib.py
@@ -1,4 +1,4 @@
-#! /usr/bin/python3
+#! /usr/bin/python
 #fib.py
 
 import sys
@@ -7,12 +7,12 @@ def fib(n):
     if (n == 0):
         return 0
     if (n == 1):
-        return 1    
-    return fib(n -1) + fib(n - 2)        
+        return 1
+    return fib(n -1) + fib(n - 2)
 
 def main():
-   if (len(sys.argv) == 2):    
-       print(fib(sys.argv[1]))   
+   if (len(sys.argv) == 2):
+       print(fib(sys.argv[1]))
    else:
        print("Usage: ", sys.argv[0], " [number]")
 
