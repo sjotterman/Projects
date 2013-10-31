85b2c245f30a5166d3024b4dd5226a0615d8d019
Samuel Otterman <sjotterman@gmail.com>
2013-10-30 22:02:10 -0400

    string_reverse is now a simple program you can call from the command line

--- Text/string_reverse/string_reverse.py
+++ Text/string_reverse/string_reverse.py
@@ -7,3 +7,10 @@ def reverse(input):
     for num in range(len(input) - 1, -1, -1):
         out_str += input[num]
     return out_str
+
+
+
+if __name__ == "__main__":
+    print("Please enter the text you want reversed.")
+    user_input = raw_input()
+    print(reverse(user_input))
