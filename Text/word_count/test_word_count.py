a5b88b67a6673d10f1372e35fa95f720545aa3b1
Samuel  Otterman <sjotterman@gmail.com>
2014-03-25 11:27:27 -0400

    added word_count folder and skeleton code files

---
+++ Text/word_count/test_word_count.py
@@ -0,0 +1,13 @@
+# test_word_count.py
+
+from __future__ import absolute_import
+import unittest
+from . import word_count
+
+class Test_word_count(unittest.TestCase):
+    def test_empty_string(self):
+        self.assertEqual(word_count.count(""), 0)
+        
+        
+if __name__ == "__main__":
+    unittest.main()
---
+++ Text/word_count/word_count.py
@@ -0,0 +1,4 @@
+# word_count.py
+
+def count(str_input):
+    return 0
