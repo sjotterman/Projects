57fd1b6fdb13a0fd2877224cd536202256da788e
Samuel Otterman <sjotterman@gmail.com>
2014-04-02 07:42:37 -0400

    Fibonacci generator passes all tests

--- Numbers/fib/fib.cpp
+++ Numbers/fib/fib.cpp
@@ -3,5 +3,7 @@
 
 int Fib::get_fib(int n)
 {
-	return 1;
+  if(n == 0){ return 0;}
+  if(n < 3){ return 1; }
+  return get_fib(n -1) + get_fib(n -2);
 }
--- Numbers/fib/tests.cpp
+++ Numbers/fib/tests.cpp
@@ -2,12 +2,37 @@
 #include "fib.h"
 #include "fib.cpp"
 
-TEST(MathTest, TwoPlusTwoEqualsFour) {
-  EXPECT_EQ(4, 2 + 2);
+
+TEST(FibTest, Fib0) {
+  EXPECT_EQ(Fib::get_fib(0), 0);
 }
 
 TEST(FibTest, Fib1) {
   EXPECT_EQ(Fib::get_fib(1), 1);
+}
+
+TEST(FibTest, Fib2) {
+  EXPECT_EQ(Fib::get_fib(2), 1);
+}
+
+TEST(FibTest, Fib3) {
+  EXPECT_EQ(Fib::get_fib(3), 2);
+}
+
+TEST(FibTest, Fib4) {
+  EXPECT_EQ(Fib::get_fib(4), 3);
+}
+
+TEST(FibTest, Fib5) {
+  EXPECT_EQ(Fib::get_fib(5), 5);
+}
+
+TEST(FibTest, Fib6) {
+  EXPECT_EQ(Fib::get_fib(6), 8);
+}
+
+TEST(FibTest, Fib20) {
+  EXPECT_EQ(Fib::get_fib(20), 6765);
 }
 
 int main(int argc, char **argv) {
